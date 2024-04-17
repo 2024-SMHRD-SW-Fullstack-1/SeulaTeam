2a41eeecc707826ad8ad837768ce69599ba3a389
kim <k26667210@gmail.com>
2024-04-17 16:08:39 +0900

    Test pullrequest

--- src/Ex01.java
+++ src/Ex01.java
@@ -6,12 +6,11 @@ public class Ex01 {
 		System.out.println("프로젝트 초안입니당!!");
 		System.out.println("팀원1 어려워요");
 		
-<<<<<<< HEAD
 		System.out.println("나만의 브렌치");
-=======
+
 		System.out.println("배고픔니다");
 		System.out.println("팀워2 뭔놈의 오류가 이렇게..");
->>>>>>> 40790257e92b505f3d5cb79b7685932c8284cdac
+
 	}
 
 }

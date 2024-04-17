8346cb7ffb6a9abb517c779972386e6cd90bec29
kim <k26667210@gmail.com>
2024-04-17 16:43:46 +0900

    branch 팀원1

--- src/Ex01.java
+++ src/Ex01.java
@@ -6,11 +6,13 @@ public class Ex01 {
 		System.out.println("프로젝트 초안입니당!!");
 		System.out.println("팀원1 어려워요");
 		
+
 		System.out.println("나만의 브렌치");
 
 		System.out.println("배고픔니다");
 		System.out.println("팀워2 뭔놈의 오류가 이렇게..");
-
+		
+		System.out.println("branch 팀원1 커밋");
 	}
 
 }
